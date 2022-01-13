a74dc9e8f311e0f944509edd19d46c90a14bdd05
ValentinaAtehortuaZ <valentinaatehortuaz@gmail.com>
2022-01-13 08:16:00 -0500

    Se agrego nuevas imagenes para la tienda

---
+++ controlador.js
@@ -0,0 +1,15 @@
+// esto es un comentario de linea
+/*esto es comentario de bloque*/
+
+// definamos variablees(ENTRADAS)
+
+let nombre="juan"
+let edad=32
+let hinchadelmejor=true
+let estatura=1.62
+
+// SALIDAS
+//console.log(nombre)
+
+nombre="jose"
+console.log(nombre)
