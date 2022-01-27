0904bc7d2691ee62bf74dcc1449f38847d0a1f53
501 <501@academicos.cesde.edu.co>
2022-01-27 11:51:54 -0500

    Se han implementado nuevos controladores y  se ha modificado la tienda

---
+++ js/ampliarInfoProducto.js
@@ -0,0 +1,11 @@
+export function ampliarInfoProducto(event){
+
+
+    
+    if(event.target.classList.contains("btn")){
+
+       
+
+    }
+
+}
--- js/controlador.js
+++ js/controlador.js
@@ -1,14 +1,24 @@
 // IMPORTACION DE ELEMENTOS
 
 
-//import{} from "./llenarTienda.js"
+import{llenarTienda} from "./llenarTienda.js"
+
+import{ampliarInfoProducto} from "./ampliarInfoProducto.js"
 
 
 // llamo a la funcion llenarTienda
 
-//llenarTienda()
+llenarTienda()
 
 
-function saludar(nombre){
-    
-}
+// Llamar la funcion ampliar info productos
+
+let modalInfoProducto = new bootstrap.Modal(document.getElementById('modalInfoProducto'))
+
+let listaProductos=document.getElementById("fila")
+listaProductos.addEventListener("click",function(event){
+
+    ampliarInfoProducto(event)
+    modalInfoProducto.show()
+   
+})
